012cc7bbc6bddd0503ff3b9b06b9561549591556
aeddung <48937254+aeddung@users.noreply.github.com>
2021-04-04 00:34:43 +0900

    KoBERT 다중분류 커스터마이징
    사용자 발화 증상에 따른 질병명을 판단하기 위해 SKT Brain의 사전학습 모델 KoBERT를 사용했고, 다중분류 모델을 구축했다.
    기본 코드 내용을 다 이해하진 못했는데, 차차 공부해야겠다...

---
+++ koBert_multiclassification.py
@@ -0,0 +1,208 @@
+# 이는 구글 코랩으로 돌린 버전입니다. 그리고 기본 코드는 SKT Brain의 KoBERT를 그대로 가져왔고, 학습 및 테스트 데이터셋만 따로 준비한 것입니다.
+# SKT Brain github 주소는 다음과 같습니다. https://github.com/SKTBrain/KoBERT
+
+!pip install mxnet
+!pip install gluonnlp pandas tqdm
+!pip install sentencepiece
+!pip install transformers==3 # 최신 버전으로 설치하면 "Input: must be Tensor, not str" 라는 에러 발생
+!pip install torch
+
+!pip install git+https://git@github.com/SKTBrain/KoBERT.git@master
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.utils.data import Dataset, DataLoader
+import gluonnlp as nlp
+import numpy as np
+from tqdm import tqdm, tqdm_notebook
+
+from kobert.utils import get_tokenizer
+from kobert.pytorch_kobert import get_pytorch_kobert_model
+
+from transformers import AdamW
+from transformers.optimization import get_cosine_schedule_with_warmup
+
+##GPU 사용 시
+device = torch.device("cuda:0")
+
+bertmodel, vocab = get_pytorch_kobert_model()
+
+from google.colab import drive
+drive.mount('/content/drive')
+
+# 학습용 데이터셋 불러오기
+import pandas as pd
+dataset_train1 = pd.read_csv('/content/drive/My Drive/Colab Notebooks/자연어처리/user_conversation.csv')
+dataset_train1.head()
+
+# 데이터 전처리
+dataset_train1.drop(['Unnamed: 0', 'Unnamed: 0.1', '질문 제목', '작성 시간', '태그', 'url'], axis=1, inplace=True)
+dataset_train1.head()
+
+dataset_train1['질병명'].unique()
+
+# 대표 질병 5개 추출
+data1 = dataset_train1[dataset_train1['질병명'] == '수족냉증']
+data2 = dataset_train1[dataset_train1['질병명'] == '식중독']
+data3 = dataset_train1[dataset_train1['질병명'] == '소화불량']
+data4 = dataset_train1[dataset_train1['질병명'] == '질염']
+data5 = dataset_train1[dataset_train1['질병명'] == '비염']
+new_data = data1.append([data2, data3, data4, data5], sort=False)
+new_data = pd.DataFrame(new_data)
+new_data.head()
+
+# 질병명 라벨링
+from sklearn.preprocessing import LabelEncoder
+encoder = LabelEncoder()
+encoder.fit(new_data['질병명'])
+new_data['질병명'] = encoder.transform(new_data['질병명'])
+new_data.head()
+
+# 라벨링된 질병명 매핑 ex) {0: '비염', 1: '소화불량', 2: '수족냉증', 3: '식중독', 4: '질염'}
+mapping = dict(zip(range(len(encoder.classes_)), encoder.classes_))
+mapping
+
+# Train / Test set 분리
+from sklearn.model_selection import train_test_split
+train, test = train_test_split(new_df, test_size=0.2, random_state=42)
+print("train shape is:", len(train))
+print("test shape is:", len(test))
+
+# 기본 Bert tokenizer 사용
+tokenizer = get_tokenizer()
+tok = nlp.data.BERTSPTokenizer(tokenizer, vocab, lower=False)
+
+class BERTDataset(Dataset):
+    def __init__(self, dataset, sent_idx, label_idx, bert_tokenizer, max_len,
+                 pad, pair):
+        transform = nlp.data.BERTSentenceTransform(
+            bert_tokenizer, max_seq_length=max_len, pad=pad, pair=pair) 
+
+        self.sentences = [transform([i[sent_idx]]) for i in dataset]
+        self.labels = [np.int32(i[label_idx]) for i in dataset]
+
+    def __getitem__(self, i):
+        return (self.sentences[i] + (self.labels[i], ))
+
+    def __len__(self):
+        return (len(self.labels))
+        
+# Setting parameters
+max_len = 64 # 해당 길이를 초과하는 단어에 대해선 bert가 학습하지 않음
+batch_size = 64
+warmup_ratio = 0.1
+num_epochs = 5
+max_grad_norm = 1
+log_interval = 200
+learning_rate = 5e-5
+
+data_train = BERTDataset(train, 0, 1, tok, max_len, True, False)
+data_test = BERTDataset(test, 0, 1, tok, max_len, True, False)
+
+# pytorch용 DataLoader 사용
+train_dataloader = torch.utils.data.DataLoader(data_train, batch_size=batch_size, num_workers=5)
+test_dataloader = torch.utils.data.DataLoader(data_test, batch_size=batch_size, num_workers=5)
+
+class BERTClassifier(nn.Module):
+    def __init__(self,
+                 bert,
+                 hidden_size = 768,
+                 num_classes = 5, # softmax 사용 <- binary일 경우는 2
+                 dr_rate=None,
+                 params=None):
+        super(BERTClassifier, self).__init__()
+        self.bert = bert
+        self.dr_rate = dr_rate
+                 
+        self.classifier = nn.Linear(hidden_size , num_classes)
+        if dr_rate:
+            self.dropout = nn.Dropout(p=dr_rate)
+    
+    def gen_attention_mask(self, token_ids, valid_length):
+        attention_mask = torch.zeros_like(token_ids)
+        for i, v in enumerate(valid_length):
+            attention_mask[i][:v] = 1
+        return attention_mask.float()
+
+    def forward(self, token_ids, valid_length, segment_ids):
+        attention_mask = self.gen_attention_mask(token_ids, valid_length)
+        
+        _, pooler = self.bert(input_ids = token_ids, token_type_ids = segment_ids.long(), attention_mask = attention_mask.float().to(token_ids.device))
+        if self.dr_rate:
+            out = self.dropout(pooler)
+        return self.classifier(out)
+      
+model = BERTClassifier(bertmodel, dr_rate=0.5).to(device)
+
+# Prepare optimizer and schedule (linear warmup and decay)
+no_decay = ['bias', 'LayerNorm.weight']
+optimizer_grouped_parameters = [
+    {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)], 'weight_decay': 0.01},
+    {'params': [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)], 'weight_decay': 0.0}
+]
+
+# 옵티마이저 선언
+optimizer = AdamW(optimizer_grouped_parameters, lr=learning_rate)
+loss_fn = nn.CrossEntropyLoss() # softmax용 Loss Function 정하기 <- binary classification도 해당 loss function 사용 가능
+
+t_total = len(train_dataloader) * num_epochs
+warmup_step = int(t_total * warmup_ratio)
+
+scheduler = get_cosine_schedule_with_warmup(optimizer, num_warmup_steps=warmup_step, num_training_steps=t_total)
+
+# 학습 평가 지표인 accuracy 계산 -> 얼마나 타겟값을 많이 맞추었는가
+def calc_accuracy(X,Y):
+    max_vals, max_indices = torch.max(X, 1)
+    train_acc = (max_indices == Y).sum().data.cpu().numpy()/max_indices.size()[0]
+    return train_acc
+  
+# 모델 학습 시작
+for e in range(num_epochs):
+    train_acc = 0.0
+    test_acc = 0.0
+    model.train()
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm_notebook(train_dataloader)):
+        optimizer.zero_grad()
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+        valid_length= valid_length
+        label = label.long().to(device)
+        out = model(token_ids, valid_length, segment_ids)
+        loss = loss_fn(out, label)
+        loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_grad_norm) # gradient clipping
+        optimizer.step()
+        scheduler.step()  # Update learning rate schedule
+        train_acc += calc_accuracy(out, label)
+        if batch_id % log_interval == 0:
+            print("epoch {} batch id {} loss {} train acc {}".format(e+1, batch_id+1, loss.data.cpu().numpy(), train_acc / (batch_id+1)))
+    print("epoch {} train acc {}".format(e+1, train_acc / (batch_id+1)))
+    
+# 테스트 문장 예측
+test_sentence = '음식을 잘못 먹었나봐요... 발열, 구토, 복통, 설사 증상이 있어요.'
+test_label = 7 # 실제 질병
+
+unseen_test = pd.DataFrame([[test_sentence, test_label]], columns = [['질문 내용', '질병명']])
+unseen_values = unseen_test.values
+test_set = BERTDataset(unseen_values, 0, 1, tok, max_len, True, False)
+test_input = torch.utils.data.DataLoader(test_set, batch_size=1, num_workers=5)
+
+for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm_notebook(test_input)):
+  token_ids = token_ids.long().to(device)
+  segment_ids = segment_ids.long().to(device)
+  valid_length= valid_length
+  out = model(token_ids, valid_length, segment_ids)
+  print(out)
+    
+    model.eval() # 평가 모드로 변경
+    
+    for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(tqdm_notebook(test_dataloader)):
+        token_ids = token_ids.long().to(device)
+        segment_ids = segment_ids.long().to(device)
+        valid_length= valid_length
+        label = label.long().to(device)
+        out = model(token_ids, valid_length, segment_ids)
+        test_acc += calc_accuracy(out, label)
+    print("epoch {} test acc {}".format(e+1, test_acc / (batch_id+1)))
